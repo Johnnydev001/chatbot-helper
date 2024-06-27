f19dd407bc45a783896c922d33831fc9b3a13ef9
João Saraiva <joao.saraiva@diconium.com>
2024-06-27 21:37:11 +0100

    Chat view
    Working on the chat view

--- frontend/src/components/chat/Chat.tsx
+++ frontend/src/components/chat/Chat.tsx
@@ -1,9 +1,15 @@
 import './chat.scss'
 import {ChatView} from "./ChatView.tsx";
+import {useState} from "react";
 export const Chat = (
     {displayChatbotIntro = false, setDisplayChatbotIntro = () => {}}) => {
+
+
+    const [inputMessage, setInputMessage] = useState<string>('')
+
+
     return (
-        <ChatView  displayChatbotIntro={displayChatbotIntro} setDisplayChatbotIntro={setDisplayChatbotIntro}/>
+        <ChatView  displayChatbotIntro={displayChatbotIntro} setDisplayChatbotIntro={setDisplayChatbotIntro} setInputMessage={setInputMessage}/>
 
     )
 }
--- frontend/src/components/chat/ChatView.tsx
+++ frontend/src/components/chat/ChatView.tsx
@@ -1,9 +1,25 @@
+import {useState} from "react";
+
 export const ChatView = (
-    {displayChatbotIntro = false, setDisplayChatbotIntro = () => {}}) => {
+    {displayChatbotIntro = false, setDisplayChatbotIntro = () => {},
+        setInputMessage = () => {}}) => {
+
+    const [messageInput, setMessageInput] = useState<string>('')
 
     const handleClick = (event: { preventDefault: () => void }) => {
         event.preventDefault()
         setDisplayChatbotIntro(!displayChatbotIntro)
+    }
+
+    const handleInputChange = (event: any) =>  {
+
+        event.preventDefault();
+        console.log('event, event', event)
+
+    }
+
+    const handleFormSubmit = () => {
+
     }
 
 
@@ -21,11 +37,16 @@ export const ChatView = (
                 <div className={'chat-container'}></div>
                 <div className={'btns-container'}>
 
-                    <input aria-label={'text input'} className={'text-input'} type={'text'} title={'Type a message'}
-                           placeholder={'Type a message'}/>
-                    <button className={'send-btn'} aria-label={'send message'} title={'Send message'}
-                            role={"button"}> Send
-                    </button>
+                    <form className={'submit-form'} onSubmit={handleFormSubmit}>
+                        <input aria-label={'text input'} className={'text-input'} type={'text'} title={'Type a message'}
+                               placeholder={'Type a message'} onChange={handleInputChange} on/>
+                        <button className={'send-btn'} aria-label={'send message'} title={'Send message'}
+                                role={"button"}> Send
+                        </button>
+
+                    </form>
+
+
                 </div>
 
 
